b45a5aefb889394e12ea9bd79274fe7847e98d25
poojith-priyavas <poojithpriyavas31@gmail.com>
2025-06-28 19:47:54 +0530

    changes

--- src/components/UpComing/UpComingMatches.jsx
+++ src/components/UpComing/UpComingMatches.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from '.UpComing/UpcomingMatches.module.css';
+import styles from './UpcomingMatches.module.css';
 
 export default function UpcomingMatches() {
     return (
