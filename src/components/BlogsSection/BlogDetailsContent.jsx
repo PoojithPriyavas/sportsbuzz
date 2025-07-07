863f32c779e7f7f3ac1b14f7607fbf90e2c4aa53
Nithin1418 <mrnithin1418@gmail.com>
2025-07-08 00:21:05 +0530

    meta tags

--- src/components/BlogsSection/BlogDetailsContent.jsx
+++ src/components/BlogsSection/BlogDetailsContent.jsx
@@ -1,16 +1,15 @@
 'use client';
 
-import { useParams } from 'next/navigation'; // For dynamic route slug
+import { useParams } from 'next/navigation';
 import { useGlobalData } from '../Context/ApiContext';
 import styles from './BlogDetailContent.module.css';
-import Image from 'next/image';
+import Head from 'next/head';
 
 export default function BlogDetailContent() {
   const { blogs } = useGlobalData();
   const params = useParams();
   const slug = params?.slug;
 
-  // Find the blog by slug
   const blog = blogs?.find((item) => item.slug === slug);
 
   if (!blog) {
@@ -18,28 +17,44 @@ export default function BlogDetailContent() {
   }
 
   return (
-    <div className={styles.blogContent}>
-      <h1 className={styles.title}>{blog.title}</h1>
+    <>
+      <Head>
+        <title>{blog.meta_title}</title>
+        <meta name="description" content={blog.meta_desc} />
+        <meta name="keywords" content={blog.tags?.join(', ')} />
+        <meta property="og:title" content={blog.meta_title} />
+        <meta property="og:description" content={blog.meta_desc} />
+        <meta property="og:image" content={blog.image_big || blog.image} />
+      </Head>
 
-      {/* Thumbnail Image */}
-      {blog.image && (
-        <div className={styles.thumbnail}>
-          <img
-            src={blog.image_big}
-            alt={blog.alt || 'Blog thumbnail'}
-            className={styles.thumbnailImg}
-          />
-        </div>
+      <div className={styles.blogContent}>
+        <h1 className={styles.title}>{blog.title}</h1>
 
-      )}
+        {blog.image && (
+          <div className={styles.thumbnail}>
+            <img
+              src={blog.image_big}
+              alt={blog.alt_big || 'Blog thumbnail'}
+              className={styles.thumbnailImg}
+            />
+          </div>
+        )}
 
-      {/* Blog Content from Rich Text Editor */}
-      <div
-        className={styles.description}
-        dangerouslySetInnerHTML={{ __html: blog.text_editor }}
-      />
+        <div
+          className={styles.description}
+          dangerouslySetInnerHTML={{ __html: blog.text_editor }}
+        />
 
-    
-    </div>
+        {/* {blog.image_big && (
+          <div className={styles.featureImage}>
+            <img
+              src={blog.image_big}
+              alt={blog.alt_big || 'Featured image'}
+              className={styles.featureImg}
+            />
+          </div>
+        )} */}
+      </div>
+    </>
   );
 }
