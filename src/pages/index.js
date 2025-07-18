71980c755f9ea287e8733c53eaf80f4b48e05b85
Nithin1418 <mrnithin1418@gmail.com>
2025-07-18 19:38:43 +0530

    index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -112,6 +112,7 @@ export default function Home() {
               <BettingCard />
               <JoinTelegramButton />
               <UpcomingFootballMatches />
+              <UpcomingMatches upcomingMatches={upcomingMatches} />
 
               <SportsOdsList />
 
