a61d2d5a2533cd0ae4ebe43690eb8700ee960d24
Nithin1418 <mrnithin1418@gmail.com>
2025-10-06 12:43:08 +0530

    header testing

--- src/components/Header/HeaderThree.jsx
+++ src/components/Header/HeaderThree.jsx
@@ -199,7 +199,7 @@ function HeaderThree({ animationStage, languageValidation }) {
             opacity: hasPlayedAnimation ? 1 : 0,
             x: 0,
             y: hasPlayedAnimation ? 0 : 80,
-            visibility: hasPlayedAnimation ? 'visible' : 'visible'
+            visibility: hasPlayedAnimation ? 'visible' : 'hidden'
         });
 
         gsap.set(navigationRef.current, {
