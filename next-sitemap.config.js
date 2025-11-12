277c738861c2d36fd9b5b33e3e64171447f4e6c0
poojith-priyavas <poojithpriyavas31@gmail.com>
2025-11-12 13:25:42 +0530

    changes in cong- sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,29 +1,48 @@
+
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://sportsbuz.com',
-  generateRobotsTxt: true,
+  siteUrl: 'https://sportsbuz.com/',
+  generateRobotsTxt: true, // (optional)
   sitemapSize: 7000,
-  exclude: ['/404', '/500'],
+  exclude: ['/404', '/500'], // optional
   additionalPaths: async (config) => {
-    return [
-      {
-        loc: '/blogs/pages/all-blogs',
+    const locales = ['en-in', 'en-de', 'en-es', 'en-pt', 'en-it', 'en-gr', 'en-nz', 'en-ae', 'en-ca', 'en-ie', 'en-pe', 'en-ar', 'en-br', 'en-gb', 'en-bd', 'en-at', 'en-hu', 'en-lk', 'en-pt', 'en-de', 'en-es',];
+    const extraPaths = [];
+
+
+    for (const locale of locales) {
+      extraPaths.push({
+        loc: `${config.siteUrl}/${locale}`,
+        changefreq: 'daily',
+        priority: 1.0,
+      });
+      extraPaths.push({
+        loc: `${config.siteUrl}/${locale}/match-schedules`,
+        changefreq: 'daily',
+        priority: 0.8,
+      });
+      extraPaths.push({
+        loc: `${config.siteUrl}/${locale}/contact`,
+        changefreq: 'monthly',
+        priority: 0.5,
+      });
+      extraPaths.push({
+        loc: `/blogs/pages/all-blogs`,
         changefreq: 'daily',
         priority: 0.9,
-        lastmod: new Date().toISOString(),
-      },
-      {
-        loc: '/best-betting-apps/current',
+      }); extraPaths.push({
+        loc: `/best-betting-apps/current`,
         changefreq: 'weekly',
         priority: 0.9,
-        lastmod: new Date().toISOString(),
-      },
-    ];
+      });
+    }
+
+    return extraPaths;
   },
   robotsTxtOptions: {
     additionalSitemaps: [
       'https://sportsbuz.com/my-custom-sitemap-1.xml',
-      'https://sportsbuz.com/sitemap.xml', // ensure main sitemap is included too
+      'https://sportsbuz.com/sitemap.xml',
     ],
   },
 };
