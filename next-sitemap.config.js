c0bb930f08720d586899f2480b28e0b055ccea23
Nithin1418 <mrnithin1418@gmail.com>
2025-11-12 12:46:00 +0530

    sitemap path fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,12 +1,29 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://sportsbuz.com/', 
-  generateRobotsTxt: true, // (optional)
+  siteUrl: 'https://sportsbuz.com',
+  generateRobotsTxt: true,
   sitemapSize: 7000,
-  exclude: ['/404', '/500'], // optional
+  exclude: ['/404', '/500'],
+  additionalPaths: async (config) => {
+    return [
+      {
+        loc: '/blogs/pages/all-blogs',
+        changefreq: 'daily',
+        priority: 0.9,
+        lastmod: new Date().toISOString(),
+      },
+      {
+        loc: '/best-betting-apps/current',
+        changefreq: 'weekly',
+        priority: 0.9,
+        lastmod: new Date().toISOString(),
+      },
+    ];
+  },
   robotsTxtOptions: {
     additionalSitemaps: [
       'https://sportsbuz.com/my-custom-sitemap-1.xml',
+      'https://sportsbuz.com/sitemap.xml', // ensure main sitemap is included too
     ],
   },
 };
