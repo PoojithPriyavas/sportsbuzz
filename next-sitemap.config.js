9b08bd8e413b5056852537a77d0487919733665e
Nithin1418 <mrnithin1418@gmail.com>
2025-11-24 17:28:35 +0530

    updates

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -39,7 +39,7 @@ module.exports = {
       priority: 0.3,
     });
     extraPaths.push({
-      loc: `${config.siteUrl}/privacy`,
+      loc: `${config.siteUrl}/policy`,
       changefreq: 'yearly',
       priority: 0.3,
     });
